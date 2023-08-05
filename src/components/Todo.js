677d169e285913bf189f4a22a5cb930ae569cac5
Rutul Tank <rutultank27@gmail.com>
2023-08-06 02:33:01 +0530

    Handled Empty task exception

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,19 +6,25 @@ import { useState } from 'react';
 const Todo = () => {
     const [addedTaskList, setAddedTaskList] = useState([]);
     const [isTaskPresent, setIsTaskPresent] = useState(false);
+    const [isTaskAdded, setIsTaskAdded] = useState(true);
 
     const updateTaskListHandler = addedTask => {
         const taskTitle = addedTask.title;
-        let isTaskDuplicate = addedTaskList.some(task => task.title === taskTitle);
-        if (!isTaskDuplicate) {
-          setIsTaskPresent(false);
-          setAddedTaskList(prevTasks => [
-          ...prevTasks,
-          { id: Math.random().toString(), ...addedTask }
-        ]);
+        if(taskTitle.length !== 0) {
+            setIsTaskAdded(true);
+            let isTaskDuplicate = addedTaskList.some(task => task.title === taskTitle);
+            if (!isTaskDuplicate) {
+                setIsTaskPresent(false);
+                setAddedTaskList(prevTasks => [
+                    ...prevTasks,
+                    { id: Math.random().toString(), ...addedTask }
+                ]);
+            } else {
+                setIsTaskPresent(true);
+                console.log('Entered Task is already present in the list');
+            }
         } else {
-            setIsTaskPresent(true);
-            console.log('Entered Task is already present in the list');
+            setIsTaskAdded(false);
         }
     };
 
@@ -32,6 +38,7 @@ const Todo = () => {
             <div className="font-bold mx-auto text-center my-5 text-3xl font-sans">My To-Do List</div>
             <TodoForm onAddTask={updateTaskListHandler}/>
             {isTaskPresent && <p className='mt-3 font-bold text-red-600'>Entered Task is already present in the list</p>}
+            {!isTaskAdded && <p className='mt-3 font-bold text-red-600'>Oops, You haven't added task</p>} 
             <TodoList taskList={addedTaskList} OnRemoveFromList={removeTaskItemFromListHandler}/>
         </div>
     )
